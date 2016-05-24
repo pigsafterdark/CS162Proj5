b8ae0454994c1cce4419f9762deeb309f599c471
pigsinspace72 <donkeykongreimer@gmail.com>
2016-05-24 16:33:53 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,13 @@
 using namespace std;
 
 char Menu();
+void Menu2A();
+char Menu2B();
+
+void Login();
+void Register();
+
+void ListBooks();
 
 int main()
 {
@@ -20,6 +27,44 @@ int main()
       switch (selection)
         {
         case 'a':
+	  Login();
+          break;
+        case 'b':
+	  Register();
+          break;
+        case 'c':
+	  terminate = true;
+          break;
+        default:
+          cout << "That is not a valid choice!\n";
+        }
+    }
+}
+
+char Menu()
+{
+  cout << endl << "a)Login" << endl;
+  cout << "b)Register" << endl;
+  cout << "c)Quit" << endl;
+
+  char selection;
+  cin >> selection;
+  return selection;
+}
+
+void Menu2A()
+{
+  bool terminate = false;
+  char selection;
+
+  while (!terminate)
+    {
+      selection = Menu2B();
+
+      switch (selection)
+        {
+        case 'a':
+	  ListBooks();
           break;
         case 'b':
           break;
@@ -38,17 +83,37 @@ int main()
     }
 }
 
-char Menu()
+char Menu2B()
 {
-  cout << endl << "a)" << endl;
+  cout << endl << "a)List all Books" << endl;
   cout << "b)" << endl;
   cout << "c)" << endl;
-  cout << "d)" << endl;
-  cout << "e)" << endl;
-  cout << "f)" << endl;
+  cout << "" << endl;
+  cout << "" << endl;
 
   char selection;
   cin >> selection;
   return selection;
 }
 
+void Login()
+{
+  cout << "Enter your name to login: ";
+  char input[256];
+  cin.ignore();
+  cin.get(input, 256, '\n');
+  cin.ignore(256, '\n');
+  cout << endl;
+
+  
+}
+
+void Register()
+{
+  cout << "Enter your first name: ";
+  char finput[256];
+  cin >> finput;
+  cout << "Enter your last name: ";
+  char linput[256];
+  cin << linput[256];
+}
