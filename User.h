035ed767568101e55ba86fe6d04c6aff05f8ead8
pigsinspace72 <donkeykongreimer@gmail.com>
2016-05-24 14:39:25 -0700

    Create User.h

---
+++ User.h
@@ -0,0 +1,12 @@
+Class User
+{
+  Private:
+    Char name_[MAX_CHAR];
+    Int numChecked_;
+ Public: 
+    Static int numUsers;
+    Char getName();
+    Void setName(char* aName);
+    Int getnumChecked();
+    void setnumChecked(numChecked);
+}
