2782ad973a4e5c04035b142d79f30424cdf70bf8
pigsinspace72 <donkeykongreimer@gmail.com>
2016-05-24 22:44:28 -0700

    Update Book.cpp

--- Book.cpp
+++ Book.cpp
@@ -23,3 +23,20 @@ void Book::setQuantity(int quantity)
 {
   quantity_ = quantity;
 }
+
+void User::PrintToConsole()
+{
+	cout << endl << "Title: " << title_ << endl;
+	cout << "Author: "author_ << endl <<; 
+	//cout << quantity_ << endl; should we print out how many there are?
+}
+
+void User::AppendToFile()
+{
+	ofstream out;
+	out.open("books.txt", ios::app);
+	out << this->title_ << ";"
+		<< this->author_ << ";"
+		<< this->quantity_ << '\n';
+	out.close();
+}
